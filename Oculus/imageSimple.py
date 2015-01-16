95cbd49b39f5f9ef3a361c66ba279dfab96eccba
Carter Fendley <carter.fendley@gmail.com>
2015-01-16 15:39:34 -0500

    Fixed most of flickering

--- Oculus/imageSimple.py
+++ Oculus/imageSimple.py
@@ -64,6 +64,10 @@ def drawContoursList(dst, inputList, contourIdx, color, thick):
 
 camera = cv2.VideoCapture(0)
 
+#Creates global variables to fix the flickering bug
+pastContours = None
+blankContours = [[], [], [], []]
+
 while(1):
 
     #Pulls a frame from the camera
@@ -79,12 +83,12 @@ while(1):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
 
     #Reshapes into a two deminsional array
-    image = image.reshape((image.shape[0] * image.shape[1], 3))
+    image2D = image.reshape((image.shape[0] * image.shape[1], 3))
 
     # apply k-means using the specified number of clusters and
     # then create the quantized image based on the predictions
     clt = MiniBatchKMeans(n_clusters = 4)
-    labels = clt.fit_predict(image)
+    labels = clt.fit_predict(image2D)
 
     #Filters the labels list into 4 peices of 2 color
     layers = listFilter(labels,0,3,0,1)
@@ -111,6 +115,13 @@ while(1):
     #Finds the contours of the output
     layerContours = findContoursList(threshLayers, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
+    #Tests if no contours then draws the last contours
+    if layerContours == blankContours:
+        if pastContours != None:
+            layerContours = pastContours
+    else:
+        pastContours = layerContours
+
     #Draws all contours onto the quant
     drawContoursList(quantifiedImage, layerContours, -1, (0,0,255), 1)
 
@@ -123,6 +134,8 @@ while(1):
     for i in range(0,len(threshLayers)):
         cv2.imshow("Thresh %s" % i, quantifiedLayers[i])
         cv2.moveWindow("Thresh %s" % i, (i*250), 0)
+    cv2.imshow("Meh", image)
+    cv2.moveWindow("Meh", 1000, 0)
 
     #Wait for 1 ms if esc pressed break main while loop
     key = cv2.waitKey(1)
