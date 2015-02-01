fa01fed410171e4c6a4c58e756ef107afc4e7e27
Ben1418 <ben.m.rice@gmail.com>
2015-02-01 11:59:03 -0500

    Webcam Detects Yellow

--- detectYellow.py
+++ detectYellow.py
@@ -10,36 +10,67 @@ def threshold_range(im, lo, hi):
     for i in range(0, len(inputList)):
         outputList.append(cv2.findContours(inputList[i].copy(), mode, method)[1])
     return outputList'''
-img = cv2.imread('Images/YT_3.jpg')
+
+
+
+cam = cv2.VideoCapture(0)
 running = True
-#while(running):
-cv2.imshow('img', img)
+while(running):
 
-#bgr = cv2.cvtColor(img, cv2.COLOR_YCrCb2BGR)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-h, s, v = cv2.split(hsv)
+    frame = cam.read()[1]
+    img = frame
 
-h = threshold_range(h, 20, 50)
-cv2.imshow('h', h)
-s = threshold_range(s, 0, 125)
-cv2.imshow('s', s)
-v = threshold_range(v, 140, 255)
-cv2.imshow('v', v)
+    #cv2.imshow('img', img)
+    oimg = img.copy()
+    #bgr = cv2.cvtColor(img, cv2.COLOR_YCrCb2BGR)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-combined = cv2.bitwise_and(h, cv2.bitwise_and(s,v))
-cv2.imshow('Combined', combined)
-img2 = combined.copy()
+    h, s, v = cv2.split(hsv)
 
-trash, contours, hierarchy = cv2.findContours(combined, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#print contours
-cv2.imshow('img2b4', img2)
-nContours = np.array()
-for contour in contours:
-    if cv2.arclength(contour, True) > 20:
-        nContours.append(contour)
+    h = threshold_range(h, 14, 59)
+    #cv2.imshow('h', h)
+    s = threshold_range(s, 57, 255)
+    #cv2.imshow('s', s)
+    v = threshold_range(v, 49, 255)
+    #cv2.imshow('v', v)
 
-a = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
-cv2.drawContours(a, nContours,-1,(0,0,255),2)
-cv2.imshow('contoury', a)
-cv2.waitKey(0)
+    combined = cv2.bitwise_and(h, cv2.bitwise_and(s,v))
+    #cv2.imshow('Combined', combined)
+    img2 = combined.copy()
+
+    trash, contours, hierarchy = cv2.findContours(combined, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+
+    #cv2.imshow('contouryb4', oimg)
+    #makes contours into polygons
+    p = []
+
+    for contour in range(0, len(contours)):
+        t = cv2.approxPolyDP(contours[contour], 100, True)
+        #print t
+        p.append(t)
+    bp = 0
+    pp = []
+    #Sorts small  groups and shows biggest polygon
+    #print len(p), 'plength'
+    for contour in range(0, len(p)):
+        ap = cv2.arcLength(p[contour], True)
+        if ap > 400:
+            pp.append(p[contour])
+
+    #print pp
+        #print ap
+    #x, y, xlen, ylen = cv2.boundingRect(pp)
+    #print pp
+    '''if cv2.arcLength(p[contour], True) > 300:
+        nContours.append(p[contour])'''
+
+
+    cv2.drawContours(oimg, pp, -1,(0,0,255), 3)
+    cv2.imshow('contoury', oimg)
+    if cv2.waitKey(1)  & 0xFF == ord('q'):
+        break
+
+cam.releas()
+cv2.destroyAllWindows()
